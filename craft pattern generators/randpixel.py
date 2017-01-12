59bd14ad4de61ce485359f166d0c88b069b2952f
Miriam Kates <quijax@gmail.com>
2017-01-12 13:58:06 -0500

    new general random pixel function

---
+++ craft pattern generators/randpixel.py
@@ -0,0 +1,12 @@
+import random
+
+def randpixel(color1,color2,percentage=50):
+"""Chooses between two different colors. If percentage is specified, 
+weight is applied to color1. That is, if you want color1 to be chosen 60%
+of the time, percentage should be 60."""
+    random.seed()
+    if random.randint(0,100) >= percent:
+		color = color2
+    else:
+		color = color1
+    return color
